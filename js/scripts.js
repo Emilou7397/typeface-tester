721ff732a6cd7bdfe07dba6fc55fa6e07a68560a
Emily Cone <econe@eagles.ewu.edu>
2019-04-18 10:39:02 -0700

    Got Fonts Working

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 $("button").click(function(e) {
 	let size = e.currentTarget.getAttribute("id");
-	let textbox = $("#font");
+	let type = e.currentTarget.parentNode.classList[1];
+	let textbox = $(`textarea#${type}`);
 	if (size === "twentyfour"){
 		textbox.css("font-size", "24pt");
 	}
